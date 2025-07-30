8b3e3f09f24726a5be0cb59bc3a9fd59617db2d6
Aditya kumar srivastav <89514025+Adi674@users.noreply.github.com>
2025-07-30 20:26:45 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(Base):
     __tablename__ = "users"
     
     user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    name = Column(String(256), nullable=False)
+    name = Column(String(256), nullable=True)
     email = Column(String(256), unique=True, nullable=False)
     phone_number = Column(BigInteger, nullable=True)
     user_type = Column(user_type_enum, default='user')
