05910eb249c4b6c2d0d840923e04c63191d231f9
manasjh1 <122570960+manasjh1@users.noreply.github.com>
2025-07-30 15:06:58 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://v0-app-sarthi-me.vercel.app"
+        "https://sarthi-frontend-six.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
